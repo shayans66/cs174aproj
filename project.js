b03175cc339f427d62011a2e68ed06830e3736d2
Alexander Sowinski <125089619+alexsowinski@users.noreply.github.com>
2023-12-08 00:31:59 -0800

    A/D keys to control bucket movement

--- project.js
+++ project.js
@@ -9,6 +9,7 @@ let spaceships = [];
 const canvas = document.getElementById("main-canvas");
 let falling_objects = [];
 let explosions = [];
+let buckets = [];
 
 const maxX = 20;
 const minX = -20;
@@ -123,6 +124,17 @@ canvas.addEventListener("click", function (event) {
             explosions.push(new Explosion(spaceships[i].shapes, spaceships[i].materials, spaceships[i].x, spaceships[i].y))
             spaceships[i].delete();
         }
+    }
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === 'a' || event.key === 'A'){
+        if (buckets[0].x > -20)
+            buckets[0].x -= 1.02;
+    }
+    if (event.key === 'd' || event.key === 'D'){
+        if (buckets[0].x < 20)
+            buckets[0].x += 1.02;
     }
 });
 
@@ -338,8 +350,7 @@ export class Project extends Scene {
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
         this.previous_time = 0;
 
-        this.buckets = [];
-        this.buckets.push(new Bucket(this.shapes, this.materials, "right", 0));
+        buckets.push(new Bucket(this.shapes, this.materials, "right", 0));
     }
 
     make_control_panel() {
@@ -391,27 +402,27 @@ export class Project extends Scene {
             spaceships[i].y += gravitydiff(spaceships[i].mass);
             spaceships[i].draw_spaceship(context, program_state, model_transform);
         }
-        for (let i = 0; i < this.buckets.length; i++) {
-            if (this.buckets[i].direction === "left") {
-                this.buckets[i].x -= .1;
+        for (let i = 0; i < buckets.length; i++) {
+           /* if (buckets[i].direction === "left") {
+                buckets[i].x -= .1;
             }
             else {
-                this.buckets[i].x += .1;
+                buckets[i].x += .1;
             }
-            if (this.buckets[i].x >= 20) {
-                this.buckets[i].direction = "left";
+            if (buckets[i].x >= 20) {
+                buckets[i].direction = "left";
             }
-            else if (this.buckets[i].x <= -20) {
-                this.buckets[i].direction = "right";
-            }
-            this.buckets[i].draw_bucket(context, program_state, model_transform);
+            else if (buckets[i].x <= -20) {
+                buckets[i].direction = "right";
+            }*/
+            buckets[i].draw_bucket(context, program_state, model_transform);
         }
 
         // Iterate through falling objects and draw them
         // console.log(falling_objects);
 
         // check for collision with bucket
-        let bucket = this.buckets[0];
+        let bucket = buckets[0];
         for (let i = 0; i < falling_objects.length; i++) {
             let obj_type = falling_objects[i].type;
             let a = falling_objects[i];
